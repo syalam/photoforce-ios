d1cdfd4acc8d39d3ea71bf0f8abc98a6871fc667
Reyaad Sidique <reyaad@tappforce.com>
2012-01-27 12:36:43 -0700

    added query to get tagged photos
    Signed-off-by: Reyaad Sidique <reyaad@tappforce.com>

--- Photoforce/HomeScreenViewController.m
+++ Photoforce/HomeScreenViewController.m
@@ -154,15 +154,15 @@
     
     NSString *getUsers = @"{'getUsers':'select uid2 from friend where uid1=me()'";
     NSString *getAlbums = @"'getAlbums':'select aid from album where owner in (select uid2 from #getUsers) order by modified desc limit 100'";
-    //NSString *getTagged = @"'getTagged':'select pid from photo_tag where subject in (select uid2 from #getUsers) order by created desc limit 100'";
+    NSString *getTagged = @"'getTagged':'select pid from photo_tag where subject in (select uid2 from #getUsers) order by created desc limit 100'";
     
-    NSString *getPics = @"'getPics':'select src_big, created, owner, caption, aid from photo where aid in (select aid from #getAlbums) order by created desc limit 100'}";
+    //NSString *getPics = @"'getPics':'select src_big, created, owner, caption, aid from photo where aid in (select aid from #getAlbums) order by created desc limit 100'}";
     
-    //NSString *getPics = @"'getPics':'select src_big, created, owner, aid from photo where aid in (select aid from #getAlbums) or pid in (select pid from #getTagged) order by created desc limit 300'}";
+    NSString *getPics = @"'getPics':'select src_big, created, owner, aid from photo where aid in (select aid from #getAlbums) or pid in (select pid from #getTagged) order by created desc limit 200'}";
     
     
-    NSString *fql = [NSString stringWithFormat:@"%@,%@,%@", getUsers, getAlbums, getPics];
-    //NSString *fql = [NSString stringWithFormat:@"%@,%@,%@,%@", getUsers, getAlbums, getTagged, getPics];
+    //NSString *fql = [NSString stringWithFormat:@"%@,%@,%@", getUsers, getAlbums, getPics];
+    NSString *fql = [NSString stringWithFormat:@"%@,%@,%@,%@", getUsers, getAlbums, getTagged, getPics];
     NSMutableDictionary *params = [NSMutableDictionary dictionaryWithObjectsAndKeys:fql, @"q", nil];
     
     [[delegate facebook] requestWithGraphPath:@"fql" andParams:params andHttpMethod:@"GET" andDelegate:self];
