92e4d2153b4698c893d5fbac77ddef825e60bcd6
Reyaad Sidique <reyaad@tappforce.com>
2012-01-30 23:12:33 -0700

    added parse framework
    Signed-off-by: Reyaad Sidique <reyaad@tappforce.com>

--- Photoforce/main.m
+++ Photoforce/main.m
@@ -7,11 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
 
 #import "AppDelegate.h"
 
 int main(int argc, char *argv[])
 {
+    [Parse setApplicationId:@"DzxqoqQythxGkL5GzJdDI38kNL6kNxNAxsHwnsiC" 
+                  clientKey:@"agksFfP0HM4q5RO4QR9iicqpw5qL3BdEeval2Gv8"];
+
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
